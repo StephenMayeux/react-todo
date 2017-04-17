9f999e9c3af9918c1ba165f27e29a5fd1c4cb1b4
Luis Diaz <luis@linuxmail.org>
2017-04-16 17:58:54 -0700

    Complete handleAddTodo

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -3,15 +3,15 @@ var React = require('react');
 var AddTodo = React.createClass({
   handleSubmit: function (e) {
     e.preventDefault();
-    var todoText = this.refs.todoText.value;
-    
+    var todoText = this.refs.todoText.value; // pull the value from the ref and store it in a new variable todoText
+
     if(todoText.length > 0) {
       this.refs.todoText.value = ''; // here we emptied the value for the ref so the user can type another input
-      this.props.onAddTodo(todoText); // here we pass in the todoText variable we created
+      this.props.onAddTodo(todoText); // here we pass in the user's input to the onAddTodo function which comes from the parent component
     } else {
-      this.refs.todoText.focus();
+      this.refs.todoText.focus(); // here we bring the cursor's focus back to the input field so the user can type something else
     }
-  },
+  }, // render value is a function that returns some jsx to be rendered
   render: function () {
     return (
       <div>
