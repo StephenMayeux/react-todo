60d3dec5b939db2fc66c14742d88852c551d05de
Luis Diaz <luis@linuxmail.org>
2017-05-04 14:48:10 -0700

    Refactor TodoSearch to work with Redux store

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,20 +1,21 @@
 var React = require('react');
 var {connect} = require('react-redux');
 import Todo from 'Todo';
+var TodoAPI = require('TodoAPI');
 
 export var TodoList = React.createClass({
   render: function () {
-    var {todos} = this.props;
+    var {todos, showCompleted, searchText} = this.props;
     var renderTodos = () => {
       if (todos.length === 0) {
         return (
           <p className="container__message">Nothing To Do</p>
         );
       }
-      return todos.map((todo) => {
-        return ( // Here we return some jsx. A Todo component for each list item
-          // The key is required by React for keeping track of instances of a component
-          <Todo key={todo.id} {...todo}/> // ... pulls out the keys/properties of the todo object (id and text)
+      return TodoAPI.filterTodos(
+          todos, showCompleted, searchText).map((todo) => {
+        return ( 
+          <Todo key={todo.id} {...todo}/>
         );
       });
     };
@@ -29,8 +30,6 @@ export var TodoList = React.createClass({
 
 export default connect(
     (state) => {
-        return {
-          todos: state.todos  
-        };
+        return state;
     }
 )(TodoList);
