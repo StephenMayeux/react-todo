fc690465ba27bb0c8c088e553d5b4a15cadc9dbb
Luis Diaz <luis@linuxmail.org>
2017-06-16 01:12:11 -0700

    Added comments to controllers file

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,12 +1,14 @@
 const jwt = require('jwt-simple');
 const User = require('../models/user');
 
-function tokenForUser(user) {
+function tokenForUser(user) { // make token by encoding user info with our salt/secret
+  // we can later verify tokens with user info and our stored secret
   const timestamp = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timestamp }, process.env.SECRET);
 }
 
-exports.signin = function(req, res, next) {
+exports.signin = function(req, res, next) { // give user their identifying token
+  // token lets them access hidden routes and data pertaining to them
   res.send({ token: tokenForUser(req.user), user: req.user });
 }
 
@@ -18,19 +20,19 @@ exports.signup = function(req, res, next) {
     return res.status(422).send({ error: 'You must provide email and password'});
   }
 
-  User.findOne({ email: email }, function(err, existingUser) {
+  User.findOne({ email: email }, function(err, existingUser) { // find user by email
     if (err) { return next(err); }
 
     if (existingUser) {
       return res.status(422).send({ error: 'Email is in use' });
     }
 
-    const user = new User({
+    const user = new User({ // create new instance of the User model/schema
       email: email,
       password: password
     });
 
-    user.save(function(err) {
+    user.save(function(err) { // save user to database and send back token
       if (err) { return next(err); }
       res.json({ token: tokenForUser(user), user: user });
     });
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json({ type: "*/*" }))
+
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
